63abc29ccd49cef77602cb3c5310aa1786bbc37d
Tanvi <tanvikaipilyawar@gmail.com>
2024-05-03 08:30:03 +0530

    PositiveandNegativeNumber

--- ex18042024/src/Lab42.java
+++ ex18042024/src/Lab42.java
@@ -1,5 +1,19 @@
+
+import java.util.Scanner;
 public class Lab42 {
     public static void main(String[] args) {
 
+        Scanner sc= new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num > 0){
+            System.out.println("Positive number");
+        } else if (num<0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+
+sc.close();
     }
 }
